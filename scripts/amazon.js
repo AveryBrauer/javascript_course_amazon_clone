fa91a1ce470904a4b4b29cfccd7af56e71280c60
AveryBrauer <averybrauer@proton.me>
2024-11-30 08:15:10 +0100

    Add variation buttons
    * adds missing products
    * adds variation buttons

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -16,16 +16,29 @@ function renderProducts(filteredProducts) {
     ? filteredProducts.map(createProductHtml).join('')
     : `<p class="empty-products-list-message">No products found.</p>`
 
+    productsGrid.querySelectorAll('.js-variation-option').forEach(button => {
+      button.addEventListener('click', () => handleVariationOptionButton(button));
+    });
+
   productsGrid.querySelectorAll('.js-add-to-cart-button').forEach(button => {
     button.addEventListener('click', () => handleAddToCart(button));
   });
 }
 
 function createProductHtml(product) {
+  console.log(product);
+  const productImage = product.image
+    ? product.image
+    : product.variations?.[0].images?.[0] || '';
+
+  const variationsHtml = product.variations
+    ? product.variations.map((variationContent, index) => createVariationHtml(variationContent, index)).join('')
+    : '';
+
   return `
-    <div class="product-container" data-product-id=${product.id}>
+    <div class="js-product-container product-container" data-product-id=${product.id}>
       <div class="product-image-container">
-        <img class="product-image" src="${product.image}">
+        <img class="js-product-image product-image" src="${productImage}" data-default-image="${productImage}">
       </div>
 
       <div class="product-name limit-to-2-lines">
@@ -51,12 +64,37 @@ function createProductHtml(product) {
         </select>
       </div>
 
+      ${variationsHtml}
+
+      <div class="product-spacer"></div>
+
       <button class="js-add-to-cart-button button-primary add-to-cart-button"
         data-product-id="${product.id}"
       >
         Add to Cart
       </button>
     </div>
+  `;
+}
+
+function createVariationHtml(variationContent, index) {
+  const buttonsHtml = variationContent.buttonsContent.map((buttonContent, index) =>
+    createVariationButtonHtml(buttonContent, index)
+  ).join('');
+
+  return `
+    <div class="variation-name">${variationContent.name}</div>
+    <div class="js-variation-options-container variation-options-container" data-index="${index}">
+      ${buttonsHtml}
+    </div>
+  `;
+}
+
+function createVariationButtonHtml(buttonContent, index) {
+  return `
+    <button class="js-variation-option variation-option" data-index="${index}">
+      ${buttonContent}
+    </button>
   `;
 }
 
@@ -79,6 +117,22 @@ function handleAddToCart(button) {
   }
   console.log('current cart: %O', cart);
   updateCartSymbol(quantity);
+}
+
+function handleVariationOptionButton(button) {
+  const buttonIndex = button.dataset.index;
+  const variationContainerIndex = button.closest('.js-variation-options-container').dataset.index;
+
+  const productContainer = button.closest('.js-product-container');
+  const productId = productContainer.dataset.productId;
+  const product = products.find(p => p.id === productId);
+
+  const newImageSource = product.variations?.[variationContainerIndex].images?.[buttonIndex] || '';
+
+  if (!newImageSource) {
+    return;
+  }
+  productContainer.querySelector('.js-product-image').src = newImageSource;
 }
 
 function updateCartSymbol(toAdd) {
